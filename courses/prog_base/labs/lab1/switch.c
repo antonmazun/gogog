690cbe5fd7c9915543c6df2f58d1658edb0f1c61
antonmazun <kpi.study1@gmail.com>
2015-10-06 17:59:22 +0300

    лаба

---
+++ courses/prog_base/labs/lab1/switch.c
@@ -0,0 +1,6 @@
+﻿1>------ Build started: Project: Project1, Configuration: Debug Win32 ------
+1>  Source.cpp
+1>c:\users\user\documents\visual studio 2013\projects\project1\project1\source.cpp(103): warning C4244: 'argument' : conversion from 'int' to 'float', possible loss of data
+1>c:\users\user\documents\visual studio 2013\projects\project1\project1\source.cpp(103): warning C4244: 'return' : conversion from 'double' to 'int', possible loss of data
+1>  Project1.vcxproj -> c:\users\user\documents\visual studio 2013\Projects\Project1\Debug\Project1.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
