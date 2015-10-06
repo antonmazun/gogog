cd2a5d41e82f3f1aedcb7bbc5b124aa75aa56d6f
antonmazun <kpi.study1@gmail.com>
2015-10-06 18:05:13 +0300

    лаба

--- courses/prog_base/labs/lab1/switch.c
+++ courses/prog_base/labs/lab1/switch.c
@@ -1,6 +1,102 @@
-﻿1>------ Build started: Project: Project1, Configuration: Debug Win32 ------
-1>  Source.cpp
-1>c:\users\user\documents\visual studio 2013\projects\project1\project1\source.cpp(103): warning C4244: 'argument' : conversion from 'int' to 'float', possible loss of data
-1>c:\users\user\documents\visual studio 2013\projects\project1\project1\source.cpp(103): warning C4244: 'return' : conversion from 'double' to 'int', possible loss of data
-1>  Project1.vcxproj -> c:\users\user\documents\visual studio 2013\Projects\Project1\Debug\Project1.exe
-========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+#include <math.h>
+int exec(int op, int a, int b)
+
+{
+
+	int c;
+
+	if (op < 0)
+	{
+		c = a;
+		a = b;
+		b = c;
+		op = abs(op);
+	}
+		switch (op){
+
+		case 0:
+				  return -a;
+				  break;
+		case 1:
+				  return a + b;
+				  break;
+		case 2:
+				   return a - b;
+				   break;   
+		case 3:
+			return a * b;
+				   break;
+		case 4:
+			if (b != 0)
+				return a / b;
+			else
+				return 0;
+			break;
+		case 5:
+			return abs(a);
+			break;
+		case 6:
+			return _Pow_int(a, b);
+			break;
+		case 7:
+		case 13:
+		case 77:
+			if (b != 0)
+				return a%b;
+			else
+				return 0;
+			break;
+		case 8:
+			return __max(a, b);
+			break;
+		case 9:
+			return __min(a, b);
+			break;
+		case 10:
+	
+			switch (abs(b) % 8){
+			case 0:
+				return abs(a)*sizeof(char);
+				break;
+			case 1:
+				return abs(a) * sizeof (signed char);
+				break;
+			case 2:
+				return abs(a) * sizeof (short);
+				break;
+			case 3:
+				return abs(a) * sizeof (unsigned int);
+				break;
+			case 4:
+				return abs(a) * sizeof (long);
+				break;
+			case 5:
+				return abs(a) * sizeof (unsigned long long);
+				break;
+			case 6:
+				return abs(a) * sizeof (float);
+				break;
+			case 7:
+				return abs(a) * sizeof (double);
+				break;
+				
+			}
+		case 11 :
+			if (a != 0)
+				return 2 * M_PI * cosf(2 * a*b) / a;
+			else
+			return 0;
+			break;
+
+		default:
+		
+				   if (op < 100)
+					   return (op % abs(a + 1)) + (op % abs(b + 1));
+				   else
+					   return -1;
+		
+			
+		}
+
+
+}
