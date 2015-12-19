c0d155de765b34c803df3d04c389be0eb8299ad6
antonmazun <kpi.study1@gmail.com>
2015-12-19 13:53:34 +0200

    Update struct2.c

--- courses/prog_base/elementary/struct2.c
+++ courses/prog_base/elementary/struct2.c
@@ -14,51 +14,51 @@ struct coord1{
 
 
 int main(void){
-	struct coord xy;
-	struct coord1 xy1;
+	struct coord *xy;
+	struct coord1 *xy1;
 	double f;
 	puts("vvedite koordinatu to4ok pryamokytnika:");
 	puts("x1");
-	scanf("%i" ,&xy.x1);
+	scanf("%i" ,&xy->x1);
 		puts("y1");
-	scanf("%i" ,&xy.y1);
+	scanf("%i" ,&xy->y1);
 		puts("x2");
-	scanf("%i" ,&xy.x2);
+	scanf("%i" ,&xy->x2);
 		puts("y2");
-	scanf("%i" ,&xy.y2);
+	scanf("%i" ,&xy->y2);
 		puts("x3");
-	scanf("%i" ,&xy.x3);
+	scanf("%i" ,&xy->x3);
 		puts("y3");
-	scanf("%i" ,&xy.y3);
+	scanf("%i" ,&xy->y3);
 		puts("x4");
-	scanf("%i" ,&xy.x4);
+	scanf("%i" ,&xy->x4);
 		puts("y4");
-	scanf("%i" ,&xy.y4);
-	xy1.x1 = xy.x1;
-	xy1.y1 = xy.y1;
-	xy1.x2 = xy.x2;
-	xy1.y2 = xy.y2;
-	xy1.x3 = xy.x3;
-	xy1.y3 = xy.y3;
-	xy1.x4 = xy.x4;
-	xy1.y4 = xy.y4;
+	scanf("%i" ,&xy->y4);
+	xy1->x1 = xy->x1;
+	xy1->y1 = xy->y1;
+	xy1->x2 = xy->x2;
+	xy1->y2 = xy->y2;
+	xy1->x3 = xy->x3;
+	xy1->y3 = xy->y3;
+	xy1->x4 = xy->x4;
+	xy1->y4 = xy->y4;
 	
-	xy.len1 = sqrt((xy.x2 - xy.x1)^2 + (xy.y2 -xy.y1 )^2);
-		xy.len2 = sqrt((xy.x3 - xy.x2)^2 + (xy.y3 -xy.y2 )^2);
-			xy.len3 = sqrt((xy.x4 - xy.x3)^2 + (xy.y4 -xy.y3 )^2);
-				xy.len4 = sqrt((xy.x1 - xy.x4)^2 + (xy.y1 -xy.y4 )^2);
-				xy1.len1 = xy.len1;
-				xy1.len2 = xy.len2;
-				xy1.len3 = xy.len3;
-				xy1.len4 = xy.len4;
-				f = ((xy.x1-xy.x2)*(xy.x3-xy.x2) + (xy.y1-xy.y2)*(xy.y3-xy.y2));
+	xy->len1 = sqrt(pow((xy->x2 - xy->x1),2) + pow((xy->y2 -xy->y1),2));
+		xy->len2 = sqrt(pow((xy->x3 - xy->x2),2) + pow((xy->y3 -xy->y2),2));
+		xy->len3 = sqrt(pow((xy->x4 - xy->x3),2) + pow((xy->y4 -xy->y3),2));
+			xy->len4 = sqrt(pow((xy->x1 - xy->x4),2) + pow((xy->y1 -xy->y4),2));
+				xy1->len1 = xy->len1;
+				xy1->len2 = xy->len2;
+				xy1->len3 = xy->len3;
+				xy1->len4 = xy->len4;
+				f = ((xy->x1-xy->x2)*(xy->x3-xy->x2) + (xy->y1-xy->y2)*(xy->y3-xy->y2));
 		
-				if(xy.len1 == xy.len3 && xy.len2 == xy.len4 && f == 0){
+				if(xy->len1 == xy->len3 && xy->len2 == xy->len4 && f == 0){
 					
-				printf("len1:%f\n" ,xy.len1);
-				printf("len2:%f\n" ,xy.len2);
-				printf("len3:%f\n" ,xy.len3);
-				printf("len4:%f\n" ,xy.len4);
+				printf("len1:%f\n" ,xy->len1);
+				printf("len2:%f\n" ,xy->len2);
+				printf("len3:%f\n" ,xy->len3);
+				printf("len4:%f\n" ,xy->len4);
 				}
 				else 
 					puts  ("It is not rectangle");
