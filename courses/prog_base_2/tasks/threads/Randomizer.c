dd9f9f04364167180950b510efeeee8531896d7c
antonmazun <kpi.study1@gmail.com>
2016-03-27 17:45:22 +0300

    Update Randomizer.c

--- courses/prog_base_2/tasks/threads/Randomizer.c
+++ courses/prog_base_2/tasks/threads/Randomizer.c
@@ -1,20 +1,32 @@
-
 #include "Randomizer.h"
-
-HANDLE hMutex;
-
+#include "threads.h"
 DWORD Randomizer(LPVOID args) {
-int * param = (int *)args;
+int * self = (int *)args;
 while (TRUE) {
 WaitForSingleObject(hMutex, INFINITE);
-(*param) = rand() % 21 - 10;
+ *self = rand() % 21 - 10;
 ReleaseMutex(hMutex);
-Sleep(1);
+Sleep(20);
 }
 }
 
 
+void randomizer_del(HANDLE * consumers){
+   for (int i = 0; i < 5; i++)
+        CloseHandle(consumers[i]);
+}
 
+HANDLE * randomizer_new(int * value){   //Указатель типа *void
+
+    HANDLE * consumers = malloc(sizeof(HANDLE) * 5);
+
+        consumers[0] = newThread(Randomizer, &value);
+        consumers[1] = newThread(Randomizer, &value);
+        consumers[2] = newThread(Randomizer, &value);
+        consumers[3] = newThread(Randomizer, &value);
+        consumers[4] = newThread(Randomizer, &value);
+    return consumers;
+}
 
 
 
