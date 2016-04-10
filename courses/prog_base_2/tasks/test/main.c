e41b27f887ed533206e3fd82cc7049bc929efa1e
antonmazun <kpi.study1@gmail.com>
2016-04-10 14:53:52 +0300

    Added files via upload

---
+++ courses/prog_base_2/tasks/test/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cJSON.h"
+#include <stdio.h>
+#include "parse.h"
+#include "struct.h"
+
+void parse(char * text, admin ** p)
+{
+	cJSON * jList = cJSON_Parse(text);
+	if (!jList) {
+		printf("%s\n", cJSON_GetErrorPtr());
+		return 1;
+
+	}
+   else{
+int count = cJSON_GetArraySize(jList);
+   for (int i = 0; i < count; i++) {
+        cJSON * jItem = cJSON_GetArrayItem(jList, i);
+        char * name = cJSON_GetObjectItem(jItem, "name")->valuestring;
+        char * surname = cJSON_GetObjectItem(jItem, "surname")->valuestring;
+        char * birthdate = cJSON_GetObjectItem(jItem, "birthdate")->valuestring;
+        cJSON * jGroup = cJSON_GetObjectItem(jItem, "working");
+        char * companyName = cJSON_GetObjectItem(jGroup, "company")->valuestring;
+        char * language = cJSON_GetObjectItem(jGroup, "language")->valuestring;
+        int  release = cJSON_GetObjectItem(jItem, "release")->valueint;
+printf("Name: %s\tSurnm:%s\t birthdate %s\t Working :\n Company :%s Language:%s Release %i sdfvbdswfvdfdsc ", name, surname, birthdate, companyName, language,release);
+// set_admin(p[i,name,surname,birthdate,company,hours);
+
+
+}
+   }
+    cJSON_Delete(jList);
+
+}
