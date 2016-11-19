77d090abc6110ce9f501fd1735fcbeee2638ac97
antonmazun <kpi.study1@gmail.com>
2016-11-19 11:32:58 +0200

    Add files via upload

---
+++ course_work/myshop/urls.py
@@ -0,0 +1,42 @@
+"""myshop URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+
+from . import settings
+
+
+
+urlpatterns = [
+# Examples:
+# url(r'^$', 'mysite.views.home', name='home'),
+# url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^basicview/', include('blog.urls')),
+    url(r'^auth/', include('loginsys.urls')),
+    url(r'^', include('blog.urls')),
+    url(r'^api/' , include('blog.api_urls')),
+   # url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+
+   # url(r'', include('user.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
